e8a26ccbf6a558ed18d41092ea8d3f81a1496b28
özer yılmaztekin <yilmaztekin.ozer@gmail.com>
2018-10-31 11:56:06 +0300

    feat: success message changed

--- extension.js
+++ extension.js
@@ -25,7 +25,7 @@ function activate(context) {
         // The code you place here will be executed every time your command is executed
         
         // Display a message box to the user
-        vscode.window.showInformationMessage('documents are generated');
+        vscode.window.showInformationMessage('documents are successfully generated');
     });
 
     context.subscriptions.push(disposable);
