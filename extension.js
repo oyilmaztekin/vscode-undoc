eb25663d03bd1dd80d244f39e096939fe22057ac
ozer <ozer@ozers-MacBook-Pro.local>
2018-11-01 00:15:56 +0300

    fix: command changed

--- extension.js
+++ extension.js
@@ -1,14 +1,16 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+
 const vscode = require('vscode');
 const Stage = require('./lib/astStages').AstStages
 
 const stages = new Stage()
 function activate(context) {
 
-    let disposable = vscode.commands.registerCommand('extension.createDocsTemplate', function () {
+    let createDocsTemplateCommand = vscode.commands.registerCommand('extension.createDocsTemplate', () => {
+        
         const activeFilePath = vscode.window.activeTextEditor.document.uri.fsPath
-    
+        
         const parsedFile = stages.parseToAst(activeFilePath)
         const transformedAst = stages.transformToAst(parsedFile)
         const generatedCode = stages.generateCode(transformedAst)
@@ -17,9 +19,10 @@ function activate(context) {
         
         // Display a message box to the user
         vscode.window.showInformationMessage('documents are successfully generated');
+        
     });
-
-    context.subscriptions.push(disposable);
+    
+    context.subscriptions.push(createDocsTemplateCommand);
 }
 exports.activate = activate;
 
