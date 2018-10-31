0efc11cdf69281696573c6eb536ff7e4144c8695
özer yılmaztekin <yilmaztekin.ozer@gmail.com>
2018-10-31 09:12:07 +0300

    feat: readme updated

--- extension.js
+++ extension.js
@@ -14,6 +14,7 @@ function activate(context) {
     const transformedAst = transformToAst(parsedFile)
     const generatedCode = generateCode(transformedAst)
     replaceFile(activeFilePath, generatedCode)
+    vscode.window.showInformationMessage('documents are generated');
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     // console.log('Congratulations, your extension "undoc" is now active!');
@@ -25,7 +26,7 @@ function activate(context) {
         // The code you place here will be executed every time your command is executed
         
         // Display a message box to the user
-        vscode.window.showInformationMessage('documents are generated');
+        //vscode.window.showInformationMessage('documents are generated');
     });
 
     context.subscriptions.push(disposable);
